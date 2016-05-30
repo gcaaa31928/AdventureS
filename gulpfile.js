74c78e71b10f9eb349f3e45a32c06198ad3a9b1b
gcaaa31928 <gcaaa31928@gmail.com>
2016-05-30 16:19:20 +0800

    Changed gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var inject = require('gulp-inject');
 var sourcemaps = require('gulp-sourcemaps');
+var mainBowerFiles = require('main-bower-files');
 var del = require('del');
 
 var paths = {
@@ -11,12 +14,12 @@ var paths = {
 
 // Not all tasks need to use streams
 // A gulpfile is just another node program and you can use any package available on npm
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     // You can use multiple globbing patterns as you would with `gulp.src`
     return del(['build']);
 });
 
-gulp.task('scripts', ['clean'], function() {
+gulp.task('scripts', ['clean'], function () {
     // Minify and copy all JavaScript (except vendor scripts)
     // with sourcemaps all the way down
     return gulp.src(paths.scripts)
@@ -25,11 +28,18 @@ gulp.task('scripts', ['clean'], function() {
         .pipe(uglify())
         .pipe(concat('all.min.js'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('build/js'))
+        .pipe(gulp.dest('app/scripts'));
+});
+
+gulp.task('index', function () {
+    return gulp.src('./app/index.html')
+        .pipe(inject(gulp.src(mainBowerFiles(), {read: false}), {name: 'bower', relative: true}))
+        .pipe(gulp.dest('./build'));
 });
 
 // Copy all static images
-gulp.task('images', ['clean'], function() {
+gulp.task('images', ['clean'], function () {
     return gulp.src(paths.images)
     // Pass in options to the task
         .pipe(imagemin({optimizationLevel: 5}))
@@ -37,9 +47,17 @@ gulp.task('images', ['clean'], function() {
 });
 
 // Rerun the task when a file changes
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(paths.scripts, ['scripts']);
 });
 
+gulp.task('start:server', function () {
+    connect.server({
+        root: ['app'],
+        livereload: false,
+        // Change this to '0.0.0.0' to access the server from outside.
+        port: 9000
+    });
+});
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'scripts', 'index', 'start:server']);
